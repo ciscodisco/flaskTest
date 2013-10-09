98209b3f0e28a5d7e81386f8fdc6aa6bbe576eb3
Ubuntu <ubuntu@ip-10-226-168-122.us-west-2.compute.internal>
2013-10-09 04:07:43 +0000

    initial files

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 from flask import * # do not use '*'; actually input the dependencies.
 from flask.ext.sqlalchemy import SQLAlchemy
 import logging
+import os
 from logging import Formatter, FileHandler
 from forms import *
 
@@ -87,12 +88,12 @@ if not app.debug:
 #----------------------------------------------------------------------------#
 
 # Default port:
+'''
 if __name__ == '__main__':
     app.run()
+'''
 
 # Or specify port manually:
-'''
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-'''
